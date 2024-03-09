1815df206fd2b3770d91fd22febf29ecf8c25397
Yaroslav <121808013+Mendeleev258@users.noreply.github.com>
2024-03-09 11:16:23 +0300

    написан main, переделать task1, доделать task2 (спросить на лабах)

--- Kaplieva_lab1_2/Kaplieva_lab1_2.cpp
+++ Kaplieva_lab1_2/Kaplieva_lab1_2.cpp
@@ -37,17 +37,80 @@ bool task1(int matr[][size]);
 bool last_digit(int x, int digit);
 void fill_null(int* arr, int size_d);
 bool condition(int* arr, int size_d, int digit);
-void shift_to_left(int* arr, int index, int size_d);
+//void task2(int** matr, int size_d, int n);
+
+// user interface
+void main_menu(int& choice);
+void source_menu(int& source);
+int exit();
 
 
 int main()
 {
-	int size_d = 4;
-	int** matrix = memory_allocation(size_d);
-	std::ifstream file("file.txt");
-	fillmatrix(matrix, file, size_d);
-	printmatrix(matrix, size_d);
-	
+	setlocale(LC_ALL, "Russian");
+	srand(time(NULL));
+	int choice, source;
+	do
+	{
+		main_menu(choice);
+		if (choice == 1)
+		{
+			int matrix[size][size];
+			std::ifstream file("file.txt");
+			std::cout << "Статическая матрица, размерность = " << size;
+			source_menu(source);
+			switch (source)
+			{
+			case 1:
+				fillmatrix(matrix, std::cin);
+				break;
+
+			case 2:
+				fillmatrix(matrix, file);
+				break;
+			default:
+				int a, b;
+				std::cout << "\nВведите левый и правый край диапазона:\n>>> ";
+				std::cin >> a >> b;
+				std::cout << '\n';
+				fillmatrix(matrix, a, b);
+				break;
+			}
+			printmatrix(matrix);
+			if (!task1(matrix))
+				std::cout << "Симметричных столбцов нет\n";
+		}
+		else
+			if (choice == 2)
+			{
+				int size_d;
+				std::cout << "Введите размер матрицы\n>>> ";
+				std::cin >> size_d;
+				int** matrix = memory_allocation(size_d);
+				std::ifstream file("file.txt");
+				source_menu(source);
+				switch (source)
+				{
+				case 1:
+					fillmatrix(matrix, std::cin, size_d);
+					break;
+
+				case 2:
+					fillmatrix(matrix, file, size_d);
+					break;
+				default:
+					int a, b;
+					std::cout << "\nВведите левый и правый край диапазона:\n>>> ";
+					std::cin >> a >> b;
+					std::cout << '\n';
+					fillmatrix(matrix, a, b, size_d);
+					break;
+				}
+				printmatrix(matrix, size_d);
+				std::cout << "<task 2>\n";
+			}
+		choice = exit();
+	} while (choice != 3);
 }
 
 
@@ -212,22 +275,47 @@ bool condition(int* arr, int size_d, int digit)
 	return res;
 }
 
-void shift_to_left(int* arr, int index, int size_d)
+//void task2(int** matr, int size_d, int n)
+//{
+//	for (int i{}; i < size_d; ++i)
+//	{
+//		if (condition(matr[i], size_d, n))
+//		{
+//			int* tmp = matr[i];
+//			for (int j = i; j < size_d - i; ++i)
+//				matr[j] = matr[j + 1];
+//			matr[size_d - 1] = tmp;
+//		}
+//	}
+//}
+
+
+
+// user interface
+void main_menu(int& choice)
 {
-	int tmp = arr[index];
-	for (int i{}; i < size_d - index - 1; ++i)
-		arr[i + index] = arr[i + index + 1];
-	arr[size_d - 1] = tmp;
+	std::cout << "\nЧто делаем?\n" <<
+		"1. Проверить, есть ли в матрице хотя бы два симметричных столбца, если есть, вывести их номера. (Ст. матрица)\n" <<
+		"2. Переместить вниз строки, содержащие числа, оканчивающиеся на заданную цифру, остальные строчки заполнить нулями.\n   Среди оставшихся строк сохранить исходный взаимный порядок. (Дин. матрица)\n" <<
+		"3. Выход из программы\n>>> ";
+	std::cin >> choice;
 }
 
-
-void task2(int* matr, int size_d, int n)
+void source_menu(int& source)
 {
-	for (int i{}; i < size_d; ++i)
-	{
-		if (condition(matr, size_d, n))
-			shift_to_left(matr, i, size_d);
-		else
-			fill_null(matr, size_d);
-	}
+	std::cout << "\nУкажите способ заполнения массива:\n" <<
+		"1. Вводом с клавиатуры\n" <<
+		"2. Чтением из файла\n" <<
+		"3. Случайным образом\n>>> ";
+	std::cin >> source;
+}
+
+int exit()
+{
+	char isExit;
+	std::cout << "\nЗавершить работу? (Y/любая клавиша) ";
+	std::cin >> isExit;
+	std::cout << '\n';
+	if (isExit == 'Y' || isExit == 'y')
+		return 3;
 }
